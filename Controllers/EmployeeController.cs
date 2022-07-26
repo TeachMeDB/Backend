7d8cc0f44bc3da3714af5c585c7ba5fcfc61ccee
vegetable-yx <1931801344@qq.com>
2022-07-26 17:18:40 +0800

    完善了前两个api
    完成异步的实现方式，添加了url的相关属性以及简单的错误处理

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -19,30 +19,31 @@ namespace youAreWhatYouEat.Controllers
 
         public class EmployeeInfo
         {
-            public decimal Id { get; set; }
+            public string Id { get; set; }
             public string? Name { get; set; }
             public string? Gender { get; set; }
             public string? Occupation { get; set; }
             public decimal? Attendance_rate { get; set; }
             public int? Award_times { get; set; }
+            public string? avatar { get; set; }
         }
 
         public class AttendInfo
         {
             public string plan_id { get; set; }
-            public DateTime? time_start { get; set; }
-            public DateTime? time_end { get; set; }
+            public string? time_start { get; set; }
+            public string? time_end { get; set; }
             public string? place { get; set; }
             public bool? attendence { get; set; }
         }
         public class PayrollInfo
         {
-            public DateTime pay_datetime { get; set; }
+            public string pay_datetime { get; set; }
             public decimal? amount { get; set; }
         }
         public class PrizeInfo
         {
-            public DateTime prize_datetime { get; set; }
+            public string prize_datetime { get; set; }
             public string? level { get; set; }
             public decimal? amount { get; set; }
         }
@@ -58,25 +59,28 @@ namespace youAreWhatYouEat.Controllers
                 data.Add("name", null);
                 data.Add("gender", null);
                 data.Add("occupation", null);
+                data.Add("avatar", null);
+                data.Add("cover", null);
             }
         }
 
         [HttpGet("GetAllEmployeeInfo")]
-        public List<EmployeeInfo> GetAllEmployeeInfo()
+        public async Task<List<EmployeeInfo>> GetAllEmployeeInfo()
         {
-            IEnumerable<Employee> employeeInfo = _context.Employees
+            var employeeInfo = await _context.Employees
                 .Include(e => e.Attends)
                 .Include(e => e.Prizes)
-                .ToList();
+                .ToListAsync();
             List<EmployeeInfo> info = new List<EmployeeInfo>();
 
             foreach (Employee employee in employeeInfo)
             {
                 EmployeeInfo tem = new EmployeeInfo();
-                tem.Id = employee.Id;
+                tem.Id = employee.Id.ToString();
                 tem.Name = employee.Name;
                 tem.Gender = employee.Gender;
                 tem.Occupation = employee.Occupation;
+                tem.avatar = System.Configuration.ConfigurationManager.AppSettings["ImagesUrl"] + "employees/employee_" + tem.Id.ToString() + ".jpg";
 
                 decimal tot = 0, participant = 0;
                 foreach (Attend item in employee.Attends)
@@ -91,30 +95,33 @@ namespace youAreWhatYouEat.Controllers
                 info.Add(tem);
             }
 
+            info.Sort((x, y) => { return x.Id.CompareTo(y.Id); });
             return info;
         }
 
         [HttpGet("GetOneEmployeeInfo/{id}")]
-        public EmployeeMessage GetOneEmployeeInfo(decimal id)
+        public async Task<EmployeeMessage> GetOneEmployeeInfo(decimal id)
         {
             EmployeeMessage message = new EmployeeMessage();
 
-            Employee employee = _context.Employees
+            var employee = await _context.Employees
                 .Include(e => e.Attends)
                     .ThenInclude(a => a.Plan)
                 .Include(e => e.Payrolls)
                 .Include(e => e.Prizes)
                     .ThenInclude(p => p.LvNavigation)
-                .SingleOrDefault(x => x.Id == id);
-            if (employee == null) return null;
-            decimal? salary = (_context.Salaries
-                .FirstOrDefault(s => s.Occupation == employee.Occupation.ToString())
-                .Amount);
+                .SingleOrDefaultAsync(x => x.Id == id);
+            if (employee == null) return message;
+            var salary = await _context.Salaries
+                .FirstOrDefaultAsync(s => s.Occupation == employee.Occupation.ToString());
+            decimal amount = (decimal)salary.Amount;
 
-            message.data["id"] = employee.Id;
+            message.data["id"] = employee.Id.ToString();
             message.data["name"] = employee.Name;
             message.data["gender"] = employee.Gender;
             message.data["occupation"] = employee.Occupation;
+            message.data["avatar"] = System.Configuration.ConfigurationManager.AppSettings["ImagesUrl"] + "employees/employee_" + employee.Id.ToString() + ".jpg";
+            message.data["cover"] = System.Configuration.ConfigurationManager.AppSettings["ImagesUrl"] + "covers/cover_" + employee.Id.ToString() + ".jpg";
             message.data.Add("attends", new List<AttendInfo>());
             message.data.Add("payrolls", new List<PayrollInfo>());
             message.data.Add("prizes", new List<PrizeInfo>());
@@ -125,8 +132,8 @@ namespace youAreWhatYouEat.Controllers
                 AttendInfo attendObj = new AttendInfo();
                 attendObj.plan_id = attend.PlanId.ToString();
                 attendObj.attendence = attend.Attendance;
-                attendObj.time_start = attend.Plan.TimeStart;
-                attendObj.time_end = attend.Plan.TimeEnd;
+                attendObj.time_start = ((DateTime)attend.Plan.TimeStart).ToString("yyyy-MM-dd hh:mm:ss");
+                attendObj.time_end = ((DateTime)attend.Plan.TimeEnd).ToString("yyyy-MM-dd hh:mm:ss");
                 attendObj.place = attend.Plan.Place;
                 attends.Add(attendObj);
             }
@@ -136,7 +143,7 @@ namespace youAreWhatYouEat.Controllers
             foreach (Prize prize in employee.Prizes)
             {
                 PrizeInfo prizesObj = new PrizeInfo();
-                prizesObj.prize_datetime = prize.PrizeDatetime;
+                prizesObj.prize_datetime = prize.PrizeDatetime.ToString("yyyy-MM-dd hh:mm:ss");
                 prizesObj.level = prize.Lv;
                 prizesObj.amount = prize.LvNavigation.Amount;
                 prizes.Add(prizesObj);
@@ -147,8 +154,8 @@ namespace youAreWhatYouEat.Controllers
             foreach (Payroll payroll in employee.Payrolls)
             {
                 PayrollInfo payrollObj = new PayrollInfo();
-                payrollObj.pay_datetime = payroll.PayDatetime;
-                payrollObj.amount = salary;
+                payrollObj.pay_datetime = payroll.PayDatetime.ToString("yyyy-MM-dd hh:mm:ss");
+                payrollObj.amount = amount;
                 payrolls.Add(payrollObj);
             }
             message.data["payrolls"] = payrolls;
