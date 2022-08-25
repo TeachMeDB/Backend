cade88c3ef43db6700f570078cfcd9453e54df3d
vegetable-yx <1931801344@qq.com>
2022-08-25 22:24:48 +0800

    小修bug

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -56,7 +56,7 @@ namespace youAreWhatYouEat.Controllers
         {
             var order = await _context.Orderlists
                 .Include(o => o.Dishorderlists)
-                .OrderBy(o => o.CreationTime)
+                .OrderByDescending(o => o.CreationTime)
                 .FirstOrDefaultAsync(o => o.TableId == table);
             if (order == null) return NoContent();
 
@@ -111,7 +111,7 @@ namespace youAreWhatYouEat.Controllers
         {
             var orders = await _context.Orderlists
                 .Include(o => o.Dishorderlists)
-                .OrderBy(o => o.CreationTime)
+                .OrderByDescending(o => o.CreationTime)
                 .ToListAsync();
             int order_count = 0;
             int awaiting_count = 0;
